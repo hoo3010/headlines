16929edf494bac854c77bff411d967db39cdaa5e
hoo3010 <hoo3010@gmail.com>
2017-05-16 22:25:55 -0400

    Using GET

--- headlines.py
+++ headlines.py
@@ -8,8 +8,8 @@ RSS_FEEDS = {'bbc' : 'http://feeds.bbci.co.uk/news/rss.xml',
              'cnn' : 'http://rss.cnn.com/rss/edition.rss',
              'fox' : 'http://feeds.foxnews.com/foxnews/latest',
              'iol' : 'http://www.iol.co.za/cmlink/1.640',
-             'bbcchinese':'http://feeds.bbci.co.uk/zhongwen/simp/rss.xml'}
-
+             'bbcchinese' : 'http://feeds.bbci.co.uk/zhongwen/simp/rss.xml',
+             'ieee' : 'http://spectrum.ieee.org/rss/fulltext'}
 @app.route("/")
 def get_news():
 	query = request.args.get("publication")
